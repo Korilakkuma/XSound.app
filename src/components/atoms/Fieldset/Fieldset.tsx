f4a82ff8587b5c5a04122b250e58f1f3badf6801
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2023-11-16 18:00:16 +0900

    feat: Add atom component for fieldset

---
+++ src/components/atoms/Fieldset/Fieldset.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export type Props = {
+  children: React.ReactNode;
+};
+
+export const Fieldset: React.FC<Props> = (props: Props) => {
+  return <fieldset className='Fieldset'>{props.children}</fieldset>;
+};
